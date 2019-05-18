1f679e29196bd9584a371612db9b8881b25f026c
ankita-04 <45062304+ankita-04@users.noreply.github.com>
2019-05-18 13:41:56 +0530

    Update Q3.java

--- Q3.java
+++ Q3.java
@@ -2,25 +2,15 @@
 public class Q3 {
 
 	public static void main(String[] args) {
-		
-		long n=600851475143L;
-		boolean k=true;
-		int i=2,fact=1;
-		for ( ;i<n;i++)
+		long n=600851475143l;
+		int i=2;
+		while (i<=n)
 		{
-			if (n%i==0 ){
-				for (int j=2;j<i;j++)
-				{
-					if (i%j==0)
-					{
-						k=false;
-					}
-				}
-				if (k){
-					System.out.println(i);
-				}
+			if (n%i==0)
+			{
+				n=n/i;
+				System.out.println("Greatest prime factor :"+i);
 			}
-			
-	}
-
+			i++;
+		}
 }}
